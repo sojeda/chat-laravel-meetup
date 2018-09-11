3e29b98ab49c32d2aa6a3f0bfd21c744e25bf4a0
Sergio Ojeda <soj3da@gmail.com>
2018-09-11 17:21:08 -0300

    Controller + Event

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'MessageController@index');
+Route::get('/messages', 'MessageController@fetch')->middleware('auth');
+Route::post('/messages', 'MessageController@sentMessage')->middleware('auth');
